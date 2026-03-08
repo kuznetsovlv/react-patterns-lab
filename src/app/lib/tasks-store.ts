bb183540a8fb152b5ec9b2b953913d80a8be9441
Leonid Kuznetsov <kuznetsovlv1981@gmail.com>
2026-03-08 21:22:30 +0400

    Fixed server

--- src/app/lib/tasks-store.ts
+++ src/app/lib/tasks-store.ts
@@ -1,20 +1,26 @@
+import {readFileSync, existsSync, writeFileSync} from 'node:fs'
+
 import type {Task} from '@/app/types';
 
-const tasks: Task[] = [];
+const filePath = './tasks.json';
 
-export const getTasks = (): Task[] => tasks;
+export const getTasks = (): Task[] => readTasks();
 
 export const addTask = (text: string): Task[] => {
+    const tasks: Task[] = getTasks();
     tasks.push({
         id: crypto.randomUUID(),
         text,
         completed: false,
     });
 
+    writeTasks(tasks);
+
     return tasks;
 };
 
 export const toggleTask = (id: string): Task | null => {
+    const tasks: Task[] = getTasks();
     const task = tasks.find((task) => task.id === id);
 
     if(!task) {
@@ -23,15 +29,32 @@ export const toggleTask = (id: string): Task | null => {
 
     task.completed = !task.completed;
 
+    writeTasks(tasks);
+
     return task;
 };
 
 export const deleteTask = (id: string): boolean => {
+    const tasks: Task[] = getTasks();
     const index = tasks.findIndex(task => task.id === id);
     if(index < 0) {
         return false;
     }
 
     tasks.splice(index, 1);
+    writeTasks(tasks);
     return true;
 };
+
+function readTasks(): Task[] {
+    if(existsSync(filePath)) {
+        const data = readFileSync(filePath, 'utf8') || '[]';
+        return JSON.parse(data) as Task[];
+    }
+    writeTasks([]);
+    return readTasks();
+}
+
+function writeTasks(tasks: Task[]): void {
+    writeFileSync(filePath, JSON.stringify(tasks), 'utf8');
+}
--- src/app/pages/TaskApp.tsx
+++ src/app/pages/TaskApp.tsx
@@ -11,8 +11,6 @@ interface TaskAppProps {
 export default memo<TaskAppProps>(function TaskApp({tasks: initialTasks}) {
     const [tasks, setTasks] = useState<Task[]>(initialTasks);
 
-
-
     const handleChangeTask = useCallback(({id, ...rest}: Task) => setTasks(tasks => tasks.map((task) => task.id === id ? {id, ...rest} : task)), []);
 
     return <>
